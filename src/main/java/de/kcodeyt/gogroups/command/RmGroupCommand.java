0600fc9c2e193c9a679b121c7f38d2cbc401da14
KCodeYT <kcodeyt@gmail.com>
2018-05-05 23:56:10 +0200

    Update RmGroupCommand, next tests, bug fixes

--- src/main/java/de/kcodeyt/gogroups/command/RmGroupCommand.java
+++ src/main/java/de/kcodeyt/gogroups/command/RmGroupCommand.java
@@ -1,11 +1,10 @@
 package de.kcodeyt.gogroups.command;
 
+import de.kcodeyt.gogroups.GoGroups;
 import io.gomint.command.Command;
 import io.gomint.command.CommandOutput;
-import io.gomint.command.annotation.Description;
-import io.gomint.command.annotation.Name;
-import io.gomint.command.annotation.Overload;
-import io.gomint.command.annotation.Permission;
+import io.gomint.command.annotation.*;
+import io.gomint.command.validator.StringValidator;
 import io.gomint.entity.EntityPlayer;
 
 import java.util.Map;
@@ -13,12 +12,26 @@ import java.util.Map;
 @Name("rmgroup")
 @Description("Removes a group")
 @Permission("gogroups.cmd.rmgroup")
-@Overload()
+@Overload({
+        @Parameter(name = "group", validator = StringValidator.class, arguments = {".*"})
+})
 public class RmGroupCommand extends Command {
 
     @Override
     public CommandOutput execute(EntityPlayer player, String s, Map<String, Object> argsMap) {
-        return null;
+        CommandOutput commandOutput = new CommandOutput();
+        GoGroups goGroups = GoGroups.getGoGroupsInstance();
+        String group = (String) argsMap.get("group");
+
+        if(group == null || group.equals(""))
+            return commandOutput.fail("Usage: /rmgroup <group>");
+
+        if(!goGroups.getGroupManager().groupExists(group))
+            return commandOutput.fail(goGroups.getFailPrefix() + " Group " + group + " does not exists.");
+
+        goGroups.getGroupManager().removeGroup(group);
+
+        return commandOutput.success(goGroups.getSuccessPrefix() + " Group " + group + " successfully removed.");
     }
 
 }
