3ba3aefa8c21ae8423a4a5938c862cec51c545b0
KCodeYT <kcodeyt@gmail.com>
2018-05-06 00:16:14 +0200

    Update SetGroupChatCommand, next tests, bug fixes

--- src/main/java/de/kcodeyt/gogroups/command/SetGroupChatCommand.java
+++ src/main/java/de/kcodeyt/gogroups/command/SetGroupChatCommand.java
@@ -1,11 +1,12 @@
 package de.kcodeyt.gogroups.command;
 
+import de.kcodeyt.gogroups.GoGroups;
+import de.kcodeyt.gogroups.config.GroupConfig;
 import io.gomint.command.Command;
 import io.gomint.command.CommandOutput;
-import io.gomint.command.annotation.Description;
-import io.gomint.command.annotation.Name;
-import io.gomint.command.annotation.Overload;
-import io.gomint.command.annotation.Permission;
+import io.gomint.command.annotation.*;
+import io.gomint.command.validator.StringValidator;
+import io.gomint.command.validator.TextValidator;
 import io.gomint.entity.EntityPlayer;
 
 import java.util.Map;
@@ -13,12 +14,31 @@ import java.util.Map;
 @Name("setgroupchat")
 @Description("Sets a chat format for a group")
 @Permission("gogroups.cmd.setgroupchat")
-@Overload()
+@Overload({
+        @Parameter(name = "group", validator = StringValidator.class, arguments = {".*"}),
+        @Parameter(name = "chatFormat", validator = TextValidator.class, arguments = {".*"})
+})
 public class SetGroupChatCommand extends Command {
 
     @Override
     public CommandOutput execute(EntityPlayer player, String s, Map<String, Object> argsMap) {
-        return null;
+        CommandOutput commandOutput = new CommandOutput();
+        GoGroups goGroups = GoGroups.getGoGroupsInstance();
+        String group = (String) argsMap.get("group");
+        String chatFormat = (String) argsMap.get("chatFormat");
+
+        if(group == null || group.equals("") || chatFormat == null || chatFormat.equals(""))
+            return commandOutput.fail("Usage: /setgroupchat <group> <chatFormat>");
+
+        if(!goGroups.getGroupManager().groupExists(group))
+            return commandOutput.fail(goGroups.getFailPrefix() + " Group " + group + " does not exists.");
+
+        GroupConfig groupConfig = goGroups.getGroupManager().getGroupsConfig().getGroupConfig(group);
+        groupConfig.setChatFormat(chatFormat);
+
+        goGroups.getGroupManager().save();
+
+        return commandOutput.success(goGroups.getSuccessPrefix() + " Chat format " + chatFormat + " successfully set to group " + group + ".");
     }
 
 }
