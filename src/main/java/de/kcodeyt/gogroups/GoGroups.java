e11984655f4e170bba685623d50451c858de54ad
KCodeYT <kcodeyt@gmail.com>
2018-05-05 20:24:33 +0200

    First Java files

---
+++ src/main/java/de/kcodeyt/gogroups/GoGroups.java
@@ -0,0 +1,21 @@
+package de.kcodeyt.gogroups;
+
+import io.gomint.plugin.Plugin;
+import io.gomint.plugin.PluginName;
+import io.gomint.plugin.Version;
+
+@PluginName("GoGroups")
+@Version(major = 1, minor = 0)
+public class GoGroups extends Plugin {
+
+    @Override
+    public void onInstall() {
+
+    }
+
+    @Override
+    public void onUninstall() {
+
+    }
+
+}
